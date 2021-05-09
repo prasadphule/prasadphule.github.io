0a65226b300519a8934d2a26f4f9a7ceadc1cfc6
prasadphule <prasadphule@live.com>
2021-05-09 20:58:32 +0000

    Deploying to gh-pages from  @ c054b3d0f20b6443d4daa0b010b53b0f154fd98a 🚀

---
+++ _next/static/3p6DjrnkGNYGJf3T2r5Mm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-15cfdbb6eafeded018d3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4a67747d593d404a38a6.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
